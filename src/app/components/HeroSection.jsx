76208451689312f9c13b4a0365b7c66e3aa7ef56
Sir Austin <42458827+austinyu@users.noreply.github.com>
2024-07-26 03:13:15 -0700

    typo

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,4 @@
-"use strict";
-"use client";
+'use client';
 
 import React from 'react';
 import Image from 'next/image';
@@ -8,16 +7,16 @@ import { TypeAnimation } from 'react-type-animation';
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className='
             text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold
-          ">
-            <span className="
+          '>
+            <span className='
               text-transparent bg-clip-text 
               bg-gradient-to-r from-purple-400 to-pink-600
-            ">
-              Hi! I'm{' '}
+            '>
+              Hi! I am Austin
             </span>
             <br />
             <TypeAnimation
@@ -31,7 +30,7 @@ const HeroSection = () => {
                 'UI/UX Designer',
                 1000
               ]}
-              wrapper="span"
+              wrapper='span'
               speed={50}
               repeat={Infinity}
             />
@@ -42,37 +41,37 @@ const HeroSection = () => {
             nunc lacinia ultricies.
           </p>
           <div>
-            <button className="
+            <button className='
               px-6 py-3 w-full sm:w-fit rounded-full mr-4
               bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500
               hover:bg-slate-200 text-white
-            ">
+            '>
               Hire me
             </button>
-            <button className="
+            <button className='
                 px-1 py-1 w-full sm:w-fit rounded-full 
                 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 
                 hover:bg-slate-800 text-white mt-3
-              ">
-              <span className="
+              '>
+              <span className='
                 block bg-[#121212] hover:bg-slate-800, rounded-full px-5 py-2
-              ">
+              '>
                 Download CV
               </span>
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-9">
-          <div className="
+        <div className='col-span-5 place-self-center mt-4 lg:mt-9'>
+          <div className='
             rounded-full bg-[#181818] relative w-[250px] h-[250px] 
             lg:w-[400px] lg:h-[400px]
-          ">
+          '>
             <Image
-              src="/images/hero-image.png"
-              alt="Hero Image"
-              className="
+              src='/images/hero-image.png'
+              alt='Hero Image'
+              className='
                 absolute transform -translate-x-1/2 
-                -translate-y-1/2 top-1/2 left-1/2"
+                -translate-y-1/2 top-1/2 left-1/2'
               width={500}
               height={500}
             />
